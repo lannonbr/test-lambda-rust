696323b2d730db5edce877f8aefbb36fa367996c
Benjamin Lannon <benjamin@lannonbr.com>
2021-11-05 11:01:28 -0400

    Test hello world rust lambda function

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use lambda_runtime::{handler_fn, Context, Error};
+use serde_json::{json, Value};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let handler_fn = handler_fn(handler);
+    lambda_runtime::run(handler_fn).await?;
+    Ok(())
+}
+
+async fn handler(_: Value, _: Context) -> Result<Value, Error> {
+    Ok(json!({"body": "🦀🦀🦀🦀🦀🦀 Hello, Rust 🦀🦀🦀🦀🦀🦀"}))
+}
